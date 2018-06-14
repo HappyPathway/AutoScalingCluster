2c163e9af214708b64db0099cfd576313fc2fb2f
Dave Arnold <darnold@hashicorp.com>
2018-06-13 22:56:43 -0700

    removing bad syntax of aws_security_group_rule.elb

--- sec_groups.tf
+++ sec_groups.tf
@@ -37,29 +37,6 @@ resource "aws_security_group_rule" "ssh" {
   cidr_blocks       = ["${var.ssh_access}"]
 }
 
-resource "aws_security_group_rule" "elb" {
-  vpc_id = "${var.vpc_id}"
-  name   = "${var.service_name}-elb"
-
-  ingress {
-    from_port   = "${var.service_port}"
-    to_port     = "${var.service_port}"
-    protocol    = "tcp"
-    cidr_blocks = ["${var.service_access}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_security_group" "elb" {
   vpc_id = "${var.vpc_id}"
   name   = "${var.service_name}-elb"
